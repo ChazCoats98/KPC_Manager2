7a01c91addc552df569d854882508eafd111ec3c
Chaz Coats <chazcoats2@hotmail.com>
2024-06-18 15:23:33 -0400

    Moving functions to their own folder for organization and readability

--- ui/views.py
+++ ui/views.py
@@ -466,104 +466,21 @@ class uploadDataForm(QWidget):
         for column in range(self.dataTable.columnCount()):
             self.dataTable.horizontalHeader().setSectionResizeMode(column, QHeaderView.Stretch)
             
-            
-        
-        
         self.setLayout(layout)
         
     def calculateCpk(self):
-        self.calculateAndUpdateCpk(self.partId)
+        functions.calculateAndUpdateCpk(self, self.partId)
         
     def onLotSizeChange(self, text):
         if text.isdigit():
             lot_size = int(text)
-            self.createLotInputs(lot_size)
+            functions.createLotInputs(self, lot_size)
         else:
-            self.clearLotInputs()
-            
-    def createLotInputs(self, lot_size):
-        currentInputs = {
-            'serialNumbers': [input.text() for input in self.serialNumberInputs],
-            'measurements': []
-        }
+            functions.clearLotInputs(self)
         
-        for table in self.featureTables:
-            currentTableData = []
-            for row in range(table.rowCount()):
-                rowData = [table.item(row, col).text() if table.item(row, col) else '' for col in range(table.columnCount())]
-                currentTableData.append(rowData)
-            currentInputs['measurements'].append(currentTableData)
-        
-        
-        self.clearLotInputs()
-        
-        partData = database.get_part_by_id(self.partId)
-        if not partData:
-            QMessageBox.warning(self, "Error", "Part data not found in database.")
-            return
-        part_features = partData['features']
-        
-        for lot_index in range(lot_size):
-            serialNumberInput = QLineEdit()
-            serialNumberInput.setPlaceholderText(f'Enter Serial Number {lot_index+1}')
-            serialNumberInput.textChanged.connect(self.checkSerialNumber)
-            self.serialNumbersLayout.addWidget(serialNumberInput)
-            self.serialNumberInputs.append(serialNumberInput)
-            
-            featureTable = QTableWidget()
-            featureTable.setColumnCount(5)
-            featureTable.setRowCount(len(part_features))
-            featureTable.setHorizontalHeaderLabels(['Feature Number', 'KPC Number', 'Blueprint Dimension', 'Op Number', 'Measurement'])
-            featureTable.horizontalHeader().setStretchLastSection(False)
-            for column in range(featureTable.columnCount()):
-                featureTable.horizontalHeader().setSectionResizeMode(column, QHeaderView.Stretch)
-                
-            featureTable.setFixedHeight(250)
-                
-            for row, feature in enumerate(part_features):
-                featureTable.setItem(row, 0, QTableWidgetItem(feature['feature']))
-                featureTable.setItem(row, 1, QTableWidgetItem(feature['kpcNum']))
-                featureTable.setItem(row, 2, QTableWidgetItem(feature['tol']))
-                featureTable.setItem(row, 3, QTableWidgetItem(feature.get('opNum', '')))
-                featureTable.setItem(row, 4, QTableWidgetItem(''))
-                
-            
-            self.adjustTableHeight(featureTable)
-            self.featureTables.append(featureTable)
-            
-            self.scrollAreaWidgetLayout.addWidget(serialNumberInput)
-            self.scrollAreaWidgetLayout.addWidget(featureTable)
-            
-        if len(currentInputs['serialNumbers']) <= lot_size:
-            for i, text in enumerate(currentInputs['serialNumbers']):
-                self.serialNumberInputs[i].setText(text)
-        else:    
-            for i, text in enumerate(currentInputs['serialNumbers'][:lot_size]):
-                self.serialNumberInputs[i].setText(text)
-                
-        if len(currentInputs['measurements']) <= lot_size:
-            for i, table_data in enumerate(currentInputs['measurements']):
-                for row, row_data in enumerate(table_data):
-                    for col, value in enumerate(row_data):
-                        self.featureTables[i].setItem(row, col, QTableWidgetItem(value))
-        else:
-            for i, table_data in enumerate(currentInputs['measurements'][:lot_size]):
-                for row, row_data in enumerate(table_data):
-                    for col, value in enumerate(row_data):
-                        self.featureTables[i].setItem(row, col, QTableWidgetItem(value))
-                
-    def clearLotInputs(self):
-        while self.scrollAreaWidgetLayout.count():
-            item = self.scrollAreaWidgetLayout.takeAt(0)
-            if item.widget():
-                item.widget().deleteLater()
-        self.serialNumberInputs = []
-        self.featureTables = []
-                    
     def checkSerialNumber(self, text):
         sender = self.sender()
         exists = database.check_serial_number(text)
-        print(exists)
         if exists:
             sender.setStyleSheet("background-color: red")
             QMessageBox.warning(self, "Serial Number Invalid", "Data for this serial number has already been uploaded.")
@@ -754,7 +671,7 @@ class uploadDataForm(QWidget):
                 QMessageBox.critical(self, "Error", f"An error occurred while saving the file: {e}")
                 
         
-        self.calculateAndUpdateCpk(self.partId)
+        functions.calculateAndUpdateCpk(self, self.partId)
     
     def loadPartData(self, selectedPartData):
         self.partNumber.setText(selectedPartData['partNumber'])
@@ -763,37 +680,6 @@ class uploadDataForm(QWidget):
         
         self.dataTable.setRowCount(0)
         self.dataTable.setHorizontalHeaderLabels(['Feature Number', 'KPC Number', 'Blueprint Dimension', 'Op Number', 'Measurement'])
-            
-    def calculateAndUpdateCpk(self, partId):
-        part_data = database.get_part_by_id(partId)
-        if part_data:
-            tolerances = {feature['kpcNum']: functions.parse_tolerance(feature.get('tol', '0-0')) for feature in part_data.get('features', [])}
-            
-        measurement_data = database.get_measurements_by_id(partId)
-        
-        measurements_by_kpc = {kpc: [] for kpc in tolerances.keys()}
-        
-        if measurement_data:
-            for entry in measurement_data:
-                for measurement in entry.get('measurements', []):
-                    kpcNum = measurement.get('kpcNum')
-                    if kpcNum and kpcNum in measurements_by_kpc:
-                        measurements_by_kpc[kpcNum].append(float(measurement['measurement']))
-        print(measurements_by_kpc)            
-        cpk_values = {}
-        for kpc, data in measurements_by_kpc.items():
-            usl, lsl = tolerances[kpc]
-            print(f'usl: {usl} lsl: {lsl}')
-            if usl is not None and lsl is not None:
-                cpk = functions.calculate_cpk(data, usl, lsl)
-                if cpk is not None:
-                    cpk_values[kpc] = cpk
-        print(cpk_values)
-                
-        if cpk_values:
-            formatted_cpk_values = {kpc: round(abs(cpk if cpk is not None else 0), 3) for kpc, cpk in cpk_values.items()}
-            database.save_cpk_values(partId, formatted_cpk_values)
-            self.dataSubmitted.emit()
         
     def closeWindow(self):
         self.close()
